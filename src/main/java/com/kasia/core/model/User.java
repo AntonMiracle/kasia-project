7c45ff35f59e6cd9ff7540e46b2887c77548a6e0
Anton Bondarenko <b.anton.m.1986@gmail.com>
2018-04-22 22:41:42 +0200

    Finish UserTest + User

--- src/main/java/com/kasia/core/model/User.java
+++ src/main/java/com/kasia/core/model/User.java
@@ -1,91 +1,51 @@
 package com.kasia.core.model;
 
-public class User {
-    /**
-    private User.Patterns NICKNAME = Patterns.NICKNAME;
-    private User.Patterns LOGIN = Patterns.LOGIN;
-    private User.Patterns PASSWORD = Patterns.PASSWORD;
-    private User.Patterns MAIL = Patterns.MAIL;
-    private long id;
-    private String nickname;
+import java.io.Serializable;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.util.Locale;
+
+public class User implements Serializable {
+    private Long id;
+    private Details details;
     private String login;
     private String password;
-    private String mail;
     private Instant createOn;
-    private ZoneId zoneId;
     private Locale locale;
-    private Details details;
+    private ZoneId zoneId;
 
-    protected User() {
+    public User() {
+
     }
 
-    public User(String login, String password, String nickname, String mail, Locale locale, ZoneId zoneId) {
-        setLogin(login);
-        setPassword(password);
-        setNickname(nickname);
-        setMail(mail);
-        setLocale(locale);
-        setZoneId(zoneId);
+    public User(User user) {
+        setId(user.getId());
+        setDetails(new Details(user.getDetails()));
+        setLogin(user.getLogin());
+        setPassword(user.getPassword());
+        setCreateOn(user.getCreateOn());
+        setLocale(user.getLocale());
+        setZoneId(user.getZoneId());
     }
 
-    protected void setId(long id) {
+    protected void setId(Long id) {
         this.id = id;
     }
 
-    protected void setNickname(String nickname) {
-        throwIAE(nickname == null, "Nickname is NULL");
-        nickname = nickname.trim();
-        throwIAE(!NICKNAME.matches(nickname), errorMsgWithPatterns(NICKNAME, nickname));
-        this.nickname = nickname;
-    }
-
-    protected void setLogin(String login) {
-        throwIAE(login == null, "login is NULL");
-        login = login.trim();
-        throwIAE(!LOGIN.matches(login), errorMsgWithPatterns(LOGIN, login));
-        this.login = login;
-    }
-
-    public void setPassword(String password) {
-        throwIAE(password == null, "Password is NULL");
-        password = password.trim();
-        throwIAE(!PASSWORD.matches(password), errorMsgWithPatterns(PASSWORD, password));
-        this.password = crypt(password);
-    }
-
-    public void setMail(String mail) {
-        throwIAE(mail == null, "Mail is NULL");
-        mail = mail.trim();
-        throwIAE(!MAIL.matches(mail), errorMsgWithPatterns(MAIL, mail));
-        this.mail = mail;
-    }
-
-    protected void setCreateOn(Instant createOn) {
-        throwIAE(createOn == null, "CreateOn is null");
-        this.createOn = createOn;
-    }
-
-    public void setZoneId(ZoneId zoneId) {
-        throwIAE(zoneId == null, "ZnoId is NULL");
-        this.zoneId = zoneId;
-    }
-
-    public void setLocale(Locale locale) {
-        throwIAE(locale == null, "Locale is NULL");
-        this.locale = locale;
-    }
-
-    public void setDetails(Details details) {
-        throwIAE(details == null, "Details is NULL");
-        this.details = new Details(details);
-    }
-
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public String getNickname() {
-        return nickname;
+    public void setDetails(Details details) {
+        this.details = details;
+    }
+
+    public Details getDetails() {
+        return details;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
     }
 
     public String getLogin() {
@@ -96,47 +56,32 @@ public class User {
         return password;
     }
 
-    public String getMail() {
-        return mail;
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setCreateOn(Instant createOn) {
+        this.createOn = createOn;
     }
 
     public Instant getCreateOn() {
         return createOn;
     }
 
-    public ZoneId getZoneId() {
-        return zoneId;
+    public void setLocale(Locale locale) {
+        this.locale = locale;
     }
 
     public Locale getLocale() {
         return locale;
     }
 
-    public Details getDetails() {
-        return details != null ? details : new Details();
+    public void setZoneId(ZoneId zoneId) {
+        this.zoneId = zoneId;
     }
 
-    protected MessageDigest getMessageDigester(String algorithmName) {
-        throwIAE(algorithmName == null, "Algorithm name is NULL");
-        try {
-            return MessageDigest.getInstance(algorithmName);
-        } catch (NoSuchAlgorithmException e) {
-            throw new IllegalArgumentException("Unknown algorithm name : " + algorithmName
-                    + "%nExceptionStackTrace : " + e);
-        }
-    }
-
-    private String crypt(String password) {
-        MessageDigest md5 = getMessageDigester("MD5");
-        md5.update(password.getBytes(), 0, password.length());
-        return new BigInteger(1, md5.digest()).toString(PASSWORD.MAX_LENGTH);
-    }
-
-    protected String errorMsgWithPatterns(Patterns patterns, String text) {
-        throwIAE(patterns == null || text == null, "Patterns or Text is NULL");
-        return "PATTERN: " + patterns.toString()
-                + " LENGTH: [" + patterns.MIN_LENGTH + "," + patterns.MAX_LENGTH + "]"
-                + "%nCURRENT: " + text;
+    public ZoneId getZoneId() {
+        return zoneId;
     }
 
     @Override
@@ -146,67 +91,64 @@ public class User {
 
         User user = (User) o;
 
-        if (id != user.id) return false;
-        if (nickname != null ? !nickname.equals(user.nickname) : user.nickname != null) return false;
+        if (id != null ? !id.equals(user.id) : user.id != null) return false;
+        if (details != null ? !details.equals(user.details) : user.details != null) return false;
         if (login != null ? !login.equals(user.login) : user.login != null) return false;
         if (password != null ? !password.equals(user.password) : user.password != null) return false;
-        if (mail != null ? !mail.equals(user.mail) : user.mail != null) return false;
         if (createOn != null ? !createOn.equals(user.createOn) : user.createOn != null) return false;
-        if (zoneId != null ? !zoneId.equals(user.zoneId) : user.zoneId != null) return false;
         if (locale != null ? !locale.equals(user.locale) : user.locale != null) return false;
-        return details != null ? details.equals(user.details) : user.details == null;
+        return zoneId != null ? zoneId.equals(user.zoneId) : user.zoneId == null;
     }
 
     @Override
     public int hashCode() {
-        int result = (int) (id ^ (id >>> 32));
-        result = 31 * result + (nickname != null ? nickname.hashCode() : 0);
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (details != null ? details.hashCode() : 0);
         result = 31 * result + (login != null ? login.hashCode() : 0);
         result = 31 * result + (password != null ? password.hashCode() : 0);
-        result = 31 * result + (mail != null ? mail.hashCode() : 0);
         result = 31 * result + (createOn != null ? createOn.hashCode() : 0);
-        result = 31 * result + (zoneId != null ? zoneId.hashCode() : 0);
         result = 31 * result + (locale != null ? locale.hashCode() : 0);
-        result = 31 * result + (details != null ? details.hashCode() : 0);
+        result = 31 * result + (zoneId != null ? zoneId.hashCode() : 0);
         return result;
     }
 
-    public boolean comparePassword(String password) {
-        throwIAE(password == null, "Password is NULL");
-        String cryptPassword = crypt(password);
-        return getPassword().equals(cryptPassword);
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", details=" + details +
+                ", login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                ", createOn=" + createOn +
+                ", locale=" + locale +
+                ", zoneId=" + zoneId +
+                '}';
     }
 
-    public enum Patterns {
-        NICKNAME("^[a-zA-Z0-9]+|([a-zA-Z0-9]+[_][a-zA-Z0-9]+)$", 5, 16),
-        LOGIN("^[a-zA-Z0-9]+|([a-zA-Z0-9]+[_][a-zA-Z0-9]+)$", 6, 16),
-        PASSWORD("^[a-zA-Z0-9\\-_@\\!\\?.]*$", 6, 16),
-        MAIL("^[^@]+@[^.@]+\\.[^.@]+$", 6, 40);
+    /**
 
-        private final int MAX_LENGTH;
-        private final int MIN_LENGTH;
-        private Patterns pattern;
+     protected MessageDigest getMessageDigester(String algorithmName) {
+     throwIAE(algorithmName == null, "Algorithm name is NULL");
+     try {
+     return MessageDigest.getInstance(algorithmName);
+     } catch (NoSuchAlgorithmException e) {
+     throw new IllegalArgumentException("Unknown algorithm name : " + algorithmName
+     + "%nExceptionStackTrace : " + e);
+     }
+     }
 
-        Patterns(String regEx, int min, int max) {
-            MAX_LENGTH = max;
-            MIN_LENGTH = min;
-            this.pattern = Patterns.compile(regEx);
-        }
+     private String crypt(String password) {
+     MessageDigest md5 = getMessageDigester("MD5");
+     md5.update(password.getBytes(), 0, password.length());
+     return new BigInteger(1, md5.digest()).toString(PASSWORD.MAX_LENGTH);
+     }
 
-        public boolean matches(String text) {
-            if (text.length() > MAX_LENGTH) return false;
-            if (text.length() < MIN_LENGTH) return false;
-            return getPattern().matcher(text).matches();
-        }
 
-        public Patterns getPattern() {
-            return pattern;
-        }
+     public boolean comparePassword(String password) {
+     throwIAE(password == null, "Password is NULL");
+     String cryptPassword = crypt(password);
+     return getPassword().equals(cryptPassword);
+     }
 
-        @Override
-        public String toString() {
-            return getPattern().pattern();
-        }
-    }
-        */
+     */
 }
--- src/test/java/com/kasia/core/model/UserTest.java
+++ src/test/java/com/kasia/core/model/UserTest.java
@@ -1,7 +1,20 @@
 package com.kasia.core.model;
 
+import nl.jqno.equalsverifier.EqualsVerifier;
+import nl.jqno.equalsverifier.Warning;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.Serializable;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.util.Locale;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class UserTest {
-    /**
     private User user;
 
     @Before
@@ -11,403 +24,101 @@ public class UserTest {
 
     @Test
     public void setAndGetId() {
-        user.setId(10);
-        assertThat(user.getId()).isEqualTo(10);
+        user.setId(new Long(10L));
+        assertThat(user.getId()).isEqualTo(10L);
     }
 
     @Test
-    public void setNicknameWithAlphabetsOnly() {
-        user.setNickname("NickName");
-        assertThat(user.getNickname()).isEqualTo("NickName");
+    public void setIdHasProtectedModifierAccess() throws NoSuchMethodException {
+        Method setId = User.class.getDeclaredMethod("setId", Long.class);
+        assertThat(Modifier.isProtected(setId.getModifiers())).isTrue();
     }
 
     @Test
-    public void setNicknameWithAlphabetsAndDownDashInMiddle() {
-        user.setNickname("Nick_Name");
-        assertThat(user.getNickname()).isEqualTo("Nick_Name");
-    }
-
-    @Test
-    public void setNicknameWithAlphabetsDownDashInMiddleAndNumbers() {
-        user.setNickname("1Nick_Name2");
-        assertThat(user.getNickname()).isEqualTo("1Nick_Name2");
-    }
-
-    @Test
-    public void setNickNameIgnoreExtraWhiteSymbolsInStartAndEnd() {
-        user.setNickname("   1Nick_Name2    ");
-        assertThat(user.getNickname()).isEqualTo("1Nick_Name2");
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void whenNickNameWithDownDashInTheEndThenIAE() {
-        user.setNickname("1Nick_Name2_");
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void whenNickNameWithDownDashInTheStartThenIAE() {
-        user.setNickname("_1Nick_Name2");
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void whenNickNameWithNotOnlyWithAZ09_ThenIAE() {
-        user.setNickname("-1Nick_Name2");
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void whenNickNameGreaterMaxLengthThenIAE() {
-        user.setNickname("NameNameNameNameD");
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void whenNickNameLowerMinLengthThenIAE() {
-        user.setNickname("Name");
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void whenSetNickNameNullThenIAE() {
-        user.setNickname(null);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void whenNickNameIsCompositeThenIAE() {
-        user.setNickname("Nick name");
-    }
-
-    @Test
-    public void whenNickNameNullGetterReturnNull() {
-        assertThat(user.getNickname()).isNull();
-    }
-
-    @Test
-    public void setLoginWithAlphabetsOnly() {
-        user.setLogin("login2");
-        assertThat(user.getLogin()).isEqualTo("login2");
-    }
-
-    @Test
-    public void setLoginWithAlphabetsAndDownDashInMiddle() {
-        user.setLogin("Lo_gin");
-        assertThat(user.getLogin()).isEqualTo("Lo_gin");
-    }
-
-    @Test
-    public void setLoginWithAlphabetsDownDashInMiddleAndNumbers() {
-        user.setLogin("1Lo_gin2");
-        assertThat(user.getLogin()).isEqualTo("1Lo_gin2");
-    }
-
-    @Test
-    public void setLoginIgnoreExtraWhiteSymbolsInStartAndEnd() {
-        user.setLogin("   1Lo_gin2    ");
-        assertThat(user.getLogin()).isEqualTo("1Lo_gin2");
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void whenLoginWithDownDashInTheEndThenIAE() {
-        user.setLogin("1Lo_gin2_");
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void whenLoginWithDownDashInTheStartThenIAE() {
-        user.setLogin("_1Lo_gin2");
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void whenLoginWithNotOnlyWithAZ09_ThenIAE() {
-        user.setLogin("-_1Lo_gin2");
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void whenLoginGreaterMaxLengthThenIAE() {
-        user.setLogin("LoginLoginLoginDD");
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void whenLoginLowerMinLengthThenIAE() {
-        user.setLogin("Logi");
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void whenSetLoginNullThenIAE() {
-        user.setLogin(null);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void whenLoginIsCompositeThenIAE() {
-        user.setLogin("Lo gin");
-    }
-
-    @Test
-    public void whenLoginNullGetterReturnNull() {
-        assertThat(user.getLogin()).isNull();
-    }
-
-    @Test
-    public void setPasswordWithAlphabetsOnly() {
-        user.setPassword("_aA09-@!?.");
-        assertThat(user.getPassword()).isNotEqualTo("_aA09-@!?.");
-    }
-
-    @Test
-    public void setPasswordIgnoreExtraWhiteSymbolsInStartAndEnd() {
-        user.setPassword("Password");
-        String crypt = user.getPassword();
-        user = new User();
-        user.setPassword("   Password    ");
-        assertThat(user.getPassword()).isEqualTo(crypt);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void whenPasswordWithWrongSymbolsThenIAE() {
-        user.setPassword("+;Password:");
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void whenPasswordGreaterMaxLengthThenIAE() {
-        user.setPassword("PassPassPassPassP");
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void whenPasswordLowerMinLengthThenIAE() {
-        user.setPassword("Passw");
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void whenSetPasswordNullThenIAE() {
-        user.setPassword(null);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void whenPasswordIsCompositeThenIAE() {
-        user.setPassword("Pass word");
-    }
-
-    @Test
-    public void whenPasswordNullGetterThenReturnNull() {
-        assertThat(user.getPassword()).isNull();
-    }
-
-    @Test
-    public void setPasswordCryptPassword() {
-        String origin = "_aA09-@!?.";
-        user.setPassword(origin);
-        String crypt = user.getPassword();
-        user = new User();
-        user.setPassword(origin);
-        assertThat(crypt).isEqualTo(user.getPassword());
-    }
-
-    @Test
-    public void setAndGetMail() {
-        user.setMail("ma@il.com");
-        assertThat(user.getMail()).isEqualTo("ma@il.com");
-    }
-
-    @Test
-    public void setMailIgnoreExtraWhiteSymbolsInStartAndEnd() {
-        user.setMail("   ma@il.com    ");
-        assertThat(user.getMail()).isEqualTo("ma@il.com");
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void whenSetMailWithoutSpecialSymbolThenIAE() {
-        user.setMail("mail.com");
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void whenSetMailWithPointAfterSpecialSymbolThenIAE() {
-        user.setMail("ma@.com");
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void whenSetMailWithoutPointAfterSpecialSymbolThenIAE() {
-        user.setMail("ma@ilcom");
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void whenSetMailWithSpecialSymbolInStartThenIAE() {
-        user.setMail("@il.com");
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void whenSetMailWithTwoSpecialSymbolInStartThenIAE() {
-        user.setMail("ma@i@l.com");
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void whenSetMailGreaterMaxLengthThenIAE() {
-        user.setMail("ma@il.commAAAAABBBBBAAAAABBBBBAAAAABBBBBd");
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void whenSetMailLowerMinLengthThenIAE() {
-        user.setMail("m@l.m");
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void whenSetMailNullThenIAE() {
-        user.setMail(null);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void whenSetMailIsCompositeThenIAE() {
-        user.setMail("Lo gin");
-    }
-
-    @Test
-    public void whenMailNullGetterReturnNull() {
-        assertThat(user.getMail()).isNull();
-    }
-
-    @Test
-    public void setAndGetCreateOn() {
-        Instant instant = Instant.now();
-        user.setCreateOn(instant);
-        assertThat(user.getCreateOn()).isEqualTo(instant);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void whenSetCreateOnWithNullTheIAE() {
-        user.setCreateOn(null);
-    }
-
-    @Test
-    public void whenCreateOnNullGetterReturnNull() {
-        assertThat(user.getCreateOn()).isNull();
-    }
-
-    @Test
-    public void setAndGetZoneId() {
-        ZoneId zoneId = ZoneId.systemDefault();
-        user.setZoneId(zoneId);
-        assertThat(user.getZoneId()).isEqualTo(zoneId);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void whenSetZoneIdWithNullTheIAE() {
-        user.setZoneId(null);
-    }
-
-    @Test
-    public void whenZoneIdNullGetterReturnNull() {
-        assertThat(user.getZoneId()).isNull();
-    }
-
-    @Test
-    public void setAndGetLocale() {
-        Locale locale = Locale.US;
-        user.setLocale(locale);
-        assertThat(user.getLocale()).isEqualTo(locale);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void whenSetLocaleWithNullTheIAE() {
-        user.setLocale(null);
-    }
-
-    @Test
-    public void whenLocaleNullGetterReturnNull() {
-        assertThat(user.getLocale()).isNull();
+    public void detailsImplementsSerializable() {
+        Serializable serializable = new User();
+        assertThat(serializable).isNotNull();
     }
 
     @Test
     public void setAndGetDetails() {
-        Details details = new Details("NAME", "SURNAME", "FIRM NAME", "POSITION");
+        user.setDetails(new Details());
+        assertThat(user.getDetails().getClass()).isEqualTo(Details.class);
+    }
+
+    @Test
+    public void setAndGetLogin() {
+        user.setLogin("login");
+        assertThat(user.getLogin()).isEqualTo("login");
+    }
+
+    @Test
+    public void setAndGetPassword() {
+        user.setPassword("pass");
+        assertThat(user.getPassword()).isEqualTo("pass");
+    }
+
+    @Test
+    public void setAndGetCreateOn() {
+        Instant now = Instant.now();
+        user.setCreateOn(now);
+        assertThat(user.getCreateOn()).isEqualTo(now);
+    }
+
+    @Test
+    public void setAndGetLocale() {
+        Locale locale = Locale.getDefault();
+        user.setLocale(locale);
+        assertThat(user.getLocale()).isEqualTo(locale);
+    }
+
+    @Test
+    public void setAndGetZoneId() {
+        ZoneId id = ZoneId.systemDefault();
+        user.setZoneId(id);
+        assertThat(user.getZoneId()).isEqualTo(id);
+    }
+
+    @Test
+    public void getUserFromAnotherUser() {
+        User user2 = new User();
+        user2.setPassword("pass");
+        user2.setZoneId(ZoneId.systemDefault());
+        user2.setLocale(Locale.getDefault());
+        user2.setLogin("login");
+        user2.setDetails(new Details());
+        user2.setCreateOn(Instant.now());
+        user2.setId(10L);
+
+        user = new User(user2);
+
+        assertThat(user.getPassword()).isEqualTo(user2.getPassword());
+        assertThat(user.getZoneId()).isEqualTo(user2.getZoneId());
+        assertThat(user.getLocale()).isEqualTo(user2.getLocale());
+        assertThat(user.getLogin()).isEqualTo(user2.getLogin());
+        assertThat(user.getDetails()).isEqualTo(user2.getDetails());
+        assertThat(user.getCreateOn()).isEqualTo(user2.getCreateOn());
+        assertThat(user.getId()).isEqualTo(user2.getId());
+    }
+
+    @Test
+    public void userInCloneConstructionUseCloneConstructionForDetails() {
+        Details details = new Details();
+        details.setName("name");
         user.setDetails(details);
-        assertThat(user.getDetails()).isEqualTo(details);
-    }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void whenSetDetailsWithNullTheIAE() {
-        user.setDetails(null);
-    }
+        user = new User(user);
 
-    @Test
-    public void whenDetailsNullGetterReturnEmptyDetails() {
-        assertThat(user.getDetails()).isEqualTo(new Details());
+        assertThat(user.getDetails().getName()).isEqualTo(details.getName());
+        details.setName("anotherName");
+        assertThat(user.getDetails().getName()).isNotEqualTo(details.getName());
     }
 
     @Test
-    public void checkEqualsAndHashCode() throws NoSuchAlgorithmException {
+    public void checkEqualsAndHashCode() {
         EqualsVerifier.forClass(User.class)
-                .withIgnoredFields("NICKNAME", "LOGIN", "PASSWORD", "MAIL")
                 .usingGetClass()
                 .suppress(Warning.NONFINAL_FIELDS)
                 .verify();
     }
-
-    @Test(expected = NullPointerException.class)
-    public void whenUserPatternsValueOfNullThenNPE() {
-        User.Patterns.valueOf(null);
-    }
-
-    @Test
-    public void getUserPatternsFromValueOf() {
-        User.Patterns patterns = User.Patterns.valueOf("NICKNAME");
-        assertThat(patterns).isSameAs(User.Patterns.NICKNAME);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void whenErrorMsgWithNullPatterns() {
-        user.errorMsgWithPatterns(null, "text");
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void whenErrorMsgWithNullText() {
-        user.errorMsgWithPatterns(User.Patterns.NICKNAME, null);
-    }
-
-    @Test
-    public void getMessageDigesterOfMD5() {
-        assertThat(user.getMessageDigester("MD5")).isNotNull();
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void whenGetMessageDigesterOfNullThenIAE() {
-        user.getMessageDigester(null);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void whenGetMessageDigesterWithUnknownThenIAE() {
-        user.getMessageDigester("SuperMd5");
-    }
-
-    @Test
-    public void whenChangeDetailsWichSetDoNotChangeUserDetails() {
-        Details details = new Details("NAME", "SURNAME", "FIRM NAME", "POSITION");
-        user.setDetails(details);
-        details.setName("SUPERNAME");
-        assertThat(user.getDetails()).isNotEqualTo(details);
-    }
-
-    @Test
-    public void createUserWithLoginPasswordNickName() {
-        String nickname = "NICKNAME";
-        String password = "PASSWORD";
-        String login = "LOGIN6";
-        String mail = "mail@mail.com";
-        ZoneId zoneId = ZoneId.of("Europe/London");
-        Locale locale = Locale.UK;
-        User user = new User(login, password, nickname, mail, locale, zoneId);
-        assertThat(user.getLogin()).isEqualTo(login);
-        assertThat(user.getPassword()).isNotEqualTo(password);
-        assertThat(user.getNickname()).isEqualTo(nickname);
-        assertThat(user.getMail()).isEqualTo(mail);
-        assertThat(user.getLocale()).isEqualTo(locale);
-        assertThat(user.getZoneId()).isEqualTo(zoneId);
-    }
-
-    @Test
-    public void compareUserPasswordWithAnotherPassword() {
-        String password = "PASSWORD";
-        user.setPassword(password);
-        assertThat(user.comparePassword(password)).isTrue();
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void whenCompareUserPasswordWithNullThenIAE() {
-        user.comparePassword(null);
-    }
-    */
 }
