0ada1193efb41f492667faa2df9d16f2c7d11be7
Anton Bondarenko <b.anton.m.1986@gmail.com>
2018-05-17 01:10:46 +0200

    Small refactoring ValidatorService

--- src/main/java/com/kasia/core/service/ValidatorService.java
+++ src/main/java/com/kasia/core/service/ValidatorService.java
@@ -11,6 +11,11 @@ import java.util.Set;
 
 
 public interface ValidatorService<T extends CoreModel> {
+    /**
+     * Get validator
+     *
+     * @return validator
+     */
     default Validator getValidator() {
         return Validation.buildDefaultValidatorFactory().getValidator();
     }
@@ -19,9 +24,17 @@ public interface ValidatorService<T extends CoreModel> {
         return getValidator().validate(model).size() == 0;
     }
 
-    default Map<String, String> mappingFieldMsg(T model) {
+
+    /**
+     * Mapping validation errors where key is field name and value error msg
+     *
+     * @param errors Set of errors
+     * @return mapped errors, where key is field name and value error msg
+     */
+    default Map<String, String> mappingFieldMsg(Set<ConstraintViolation<T>> errors) {
         Map<String, String> result = new HashMap<>();
-        Set<ConstraintViolation<T>> errors = getValidator().validate(model);
+
+        if (errors == null) return result;
 
         for (ConstraintViolation<T> error : errors) {
             result.put(error.getPropertyPath().toString(), error.getMessage());
@@ -29,11 +42,17 @@ public interface ValidatorService<T extends CoreModel> {
         return result;
     }
 
-    void setDefaultValuesIfNull(T model);
+    /**
+     * If and only if {@link T} is null then return new {@link T} where {@link T#isNull()} return true
+     * Otherwise set model fields, where acceptable to switch null on default value
+     *
+     * @param model
+     */
+    void setAcceptDefaultValue(T model);
 
     /**
-     * If and only if {@param model} is valid then {@link T#isValid()} return true
-     * If {@param model} is null then {@link T#isNull()} return true
+     * If and only if {@link T} is valid then {@link T#isValid()} return true
+     * If and only if {@link T} is null then {@link T#isNull()} return true
      *
      * @param model to validate
      * @return model after validation
