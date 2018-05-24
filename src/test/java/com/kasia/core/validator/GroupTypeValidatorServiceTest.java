405444543b25b28baaa09b5c9d73d5b6eda127e1
Anton Bondarenko <b.anton.m.1986@gmail.com>
2018-05-25 00:25:43 +0200

    Fix compiled errors GroupTypeValidatorServiceTest

--- src/test/java/com/kasia/core/validator/GroupTypeValidatorServiceTest.java
+++ src/test/java/com/kasia/core/validator/GroupTypeValidatorServiceTest.java
@@ -3,6 +3,7 @@ package com.kasia.core.validator;
 import com.kasia.config.AppConfig;
 import com.kasia.core.TestHelper;
 import com.kasia.core.model.GroupType;
+import com.kasia.core.model.Result;
 import com.kasia.core.service.ValidatorService;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,6 +11,10 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.validation.ConstraintViolation;
+import java.util.Map;
+import java.util.Set;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -25,39 +30,75 @@ public class GroupTypeValidatorServiceTest implements TestHelper<GroupType> {
     public void before() {
         assert validator != null;
         groupType = new GroupType();
-        groupType.setName("Name");
     }
 
-    //name ----------------------------------------------------------
+    //validation ===================================================
     @Test
     public void nameWithEmptyStringInvalid() {
         groupType.setName("");
-        assertThat(validator.isValid(groupType)).isFalse();
+
+        Result<Boolean> result = validator.validation(groupType);
+
+        assertThat(result.isCalculationFailed()).isFalse();
+        assertThat(result.getResult()).isFalse();
     }
 
     @Test
     public void nameWithLengthMoreThan32Invalid() {
         groupType.setName("ggggggggggggggggggggggggggggggggG");
-        assertThat(validator.isValid(groupType)).isFalse();
+
+        Result<Boolean> result = validator.validation(groupType);
+
+        assertThat(result.isCalculationFailed()).isFalse();
+        assertThat(result.getResult()).isFalse();
     }
 
     @Test
     public void nameWithNullInvalid() {
         groupType.setName(null);
-        assertThat(validator.isValid(groupType)).isFalse();
+
+        Result<Boolean> result = validator.validation(groupType);
+
+        assertThat(result.isCalculationFailed()).isFalse();
+        assertThat(result.getResult()).isFalse();
     }
 
-    //id ----------------------------------------------------------
-    @Test
-    public void idWithNullValid() throws NoSuchFieldException, IllegalAccessException {
-        assertThat(groupType.getId()).isNull();
-        assertThat(validator.isValid(groupType)).isTrue();
+    @Test(expected = NullPointerException.class)
+    public void whenValidationWithNullThenNPE() {
+        validator.validation(null);
     }
 
-    //mapping error by field name and error msg -----------------------------------------
+    //mapping error by field name and error msg =============================================
     @Test
-    public void groupHasNameWithNullMapWithErrorHasSize1() {
+    public void groupHasNameWithNullMapDieldMsgWithHasSize1() {
         groupType.setName(null);
-        assertThat(validator.mappingFieldMsg(groupType).size()).isEqualTo(1);
+
+        Set<ConstraintViolation<GroupType>> errors = validator.getValidator().validate(groupType);
+        Result<Map<String, String>> result = validator.mapFieldMsg(errors);
+
+        assertThat(result.isCalculationFailed()).isFalse();
+        assertThat(result.getResult().size()).isEqualTo(1);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void whenMapFieldMsgWithNullThenNPE() {
+        validator.mapFieldMsg(null);
+    }
+
+    // eliminate null value =====================================================================
+    @Test
+    public void setAcceptDefaultValueSetIsNullFalse() {
+        groupType = new GroupType();
+        assertThat(groupType.getName()).isNull();
+
+        Result<GroupType> result = validator.eliminateNull(groupType);
+
+        assertThat(result.isCalculationFailed()).isFalse();
+        assertThat(result.getResult().getName()).isNotNull();
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void whenSetAcceptDefaultWithNullThenNPE() {
+        validator.eliminateNull(null);
     }
 }
