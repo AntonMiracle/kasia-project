c29e94d4329d5e1a5e707cc74e62ce5c2ca65e16
Anton Bondarenko <b.anton.m.1986@gmail.com>
2018-05-28 23:39:26 +0200

    Fix GroupTypeServiceTest

--- src/test/java/com/kasia/core/service/GroupTypeServiceTest.java
+++ src/test/java/com/kasia/core/service/GroupTypeServiceTest.java
@@ -3,6 +3,7 @@ package com.kasia.core.service;
 import com.kasia.config.AppConfig;
 import com.kasia.core.TestHelper;
 import com.kasia.core.model.GroupType;
+import com.kasia.core.model.Result;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,7 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import javax.validation.ConstraintViolationException;
+import java.util.Set;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
@@ -21,168 +22,268 @@ public class GroupTypeServiceTest implements TestHelper<GroupType> {
     @Autowired
     private GroupTypeService service;
     private GroupType groupType;
+    private String name1;
+    private String name2;
 
     @Before
     public void before() {
         assert service != null;
-        groupType = new GroupType();
+        name1 = getGroupTypeNameForTesting1();
+        name2 = getGroupTypeNameForTesting2();
     }
 
     @After
     public void after() {
-        if (service.isNameExist(getGroupTypeNameForTesting1())) {
-            service.delete(getGroupTypeNameForTesting1());
+        Result<Boolean> result = service.isNameExist(name1);
+        if (!result.isCalculationFailed() && result.getResult()) {
+            service.delete(getRoleNameForTesting1());
         }
-        if (service.isNameExist(getGroupTypeNameForTesting2())) {
-            service.delete(getGroupTypeNameForTesting2());
+
+        result = service.isNameExist(name2);
+        if (!result.isCalculationFailed() && result.getResult()) {
+            service.delete(getRoleNameForTesting2());
         }
     }
 
-    // SAVE OR UPDATE --------------------------------------------------------
+    // SAVE ==================================================================
     @Test
-    public void saveOrUpdate() {
-        groupType.setName(getGroupTypeNameForTesting1());
-        assertThat(groupType.getId()).isNull();
+    public void saveUniqueSuccess() {
+        groupType = new GroupType();
+        groupType.setName(name1);
+        Result<GroupType> result = service.save(groupType);
 
-        groupType = service.saveOrUpdate(groupType);
-        assertThat(groupType.getId()).isNotNull();
+        assertThat(result.isCalculationFailed()).isFalse();
+        assertThat(result.getResult().getId()).isNotNull();
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void whenSaveOrUpdateWithNotUniqueNameThenIAE() {
-        groupType.setName(getGroupTypeNameForTesting1());
-        service.saveOrUpdate(groupType);
+    @Test
+    public void saveNotUniqueFailed() {
+        groupType = new GroupType();
+        groupType.setName(name1);
+
+        Result<GroupType> result = service.save(groupType);
+        assertThat(result.isCalculationFailed()).isFalse();
+        assertThat(result.getResult()).isNull();
 
         groupType = new GroupType();
-        groupType.setName(getGroupTypeNameForTesting1());
-        service.saveOrUpdate(groupType);
+        groupType.setName(name1);
+
+        result = service.save(groupType);
+        assertThat(result.isCalculationFailed()).isTrue();
+        assertThat(result.getResult()).isNotNull();
     }
 
-    @Test(expected = ConstraintViolationException.class)
-    public void whenSaveOrUpdateWithInValidNameThenCVE() {
-        groupType.setName("");
-        assertThat(groupType.getId()).isNull();
-
-        groupType = service.saveOrUpdate(groupType);
-    }
-
-    @Test(expected = ConstraintViolationException.class)
-    public void whenSaveOrUpdateWithNullNameThenCVE() {
-        groupType.setName(null);
-        assertThat(groupType.getId()).isNull();
-
-        groupType = service.saveOrUpdate(groupType);
-    }
 
     @Test(expected = NullPointerException.class)
-    public void whenSaveOrUpdateWithNullThenNPE() {
-        groupType = service.saveOrUpdate(null);
+    public void whenSaveNullThenNPE() {
+        service.save(null);
     }
 
-    // GET by ID --------------------------------------------------------
+    // UPDATE =======================================================
     @Test
-    public void getById() {
-        groupType.setName(getGroupTypeNameForTesting1());
-        Long id = service.saveOrUpdate(groupType).getId();
+    public void updateExistSuccess() {
+        groupType = new GroupType();
+        groupType.setName(name1);
 
-        groupType = service.get(id);
+        Result<GroupType> result = service.save(groupType);
+        assertThat(result.isCalculationFailed()).isFalse();
 
-        assertThat(groupType.getId()).isEqualTo(id);
+        groupType = result.getResult();
+        groupType.setName(name2);
+
+        result = service.update(groupType);
+
+        assertThat(result.isCalculationFailed()).isFalse();
+        assertThat(result.getResult().getId()).isEqualTo(groupType.getId());
+    }
+
+    @Test
+    public void updateNotExistFailed() {
+        groupType = new GroupType();
+        groupType.setName(name1);
+
+        Result<GroupType> result = service.update(groupType);
+
+        assertThat(result.isCalculationFailed()).isTrue();
+        assertThat(result.getResult()).isNull();
     }
 
     @Test(expected = NullPointerException.class)
-    public void whenGetByIdWithNullThenNPE() {
+    public void whenUpdateNullThenNPE() {
+        service.update(null);
+    }
+
+    // GET by ID ================================================================
+    @Test
+    public void getByExistIdSuccess() {
+        groupType = new GroupType();
+        groupType.setName(name1);
+
+        Result<GroupType> result = service.save(groupType);
+        assertThat(result.isCalculationFailed()).isFalse();
+
+        long id = result.getResult().getId();
+
+        result = service.get(id);
+        assertThat(result.isCalculationFailed()).isFalse();
+
+        assertThat(result.getResult().getId()).isEqualTo(id);
+    }
+
+    @Test
+    public void getByNotExistIdFailed() {
+        Result<GroupType> result = service.get(-1L);
+        assertThat(result.isCalculationFailed()).isTrue();
+        assertThat(result.getResult()).isNull();
+
+        result = service.get(0L);
+        assertThat(result.isCalculationFailed()).isTrue();
+        assertThat(result.getResult()).isNull();
+        ;
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void whenGetByIdNullThenNPE() {
         service.get((Long) null);
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void whenGetByIdWithNotExistIdThenIAE() {
-        service.get(10L);
+    // DELETE by ID ===================================================================
+    @Test
+    public void deleteByExistIdSuccess() {
+        groupType = new GroupType();
+        groupType.setName(name1);
+
+        Result<GroupType> result = service.save(groupType);
+        assertThat(result.isCalculationFailed()).isFalse();
+
+        long id = result.getResult().getId();
+
+        Result<Boolean> resultBoolean = service.delete(id);
+        assertThat(resultBoolean.isCalculationFailed()).isFalse();
+
+        assertThat(resultBoolean.getResult()).isTrue();
     }
 
-    // DELETE by ID --------------------------------------------------------
     @Test
-    public void deleteById() {
-        groupType.setName(getGroupTypeNameForTesting1());
-        Long id = service.saveOrUpdate(groupType).getId();
+    public void deleteByNotExistIdFailed() {
+        Result<Boolean> resultBoolean = service.delete(-1L);
+        assertThat(resultBoolean.isCalculationFailed()).isFalse();
 
-        assertThat(service.delete(id)).isTrue();
+        assertThat(resultBoolean.getResult()).isTrue();
     }
 
     @Test(expected = NullPointerException.class)
-    public void whenDeleteByIdWithNullThenNPE() {
+    public void whenDeleteByIdNullThenNPE() {
         service.delete((Long) null);
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void whenDeleteByIdWithNotExistIdThenIAE() {
-        service.delete(-10L);
-    }
-
-    // GET ALL --------------------------------------------------------
+    // GET ALL ===========================================================
     @Test
     public void getAll() {
-        int size = service.get().size();
+        Result<Set<GroupType>> resultAll = service.get();
+        assertThat(resultAll.isCalculationFailed()).isFalse();
+        long size = resultAll.getResult().size();
 
-        groupType.setName(getGroupTypeNameForTesting1());
-        service.saveOrUpdate(groupType).getName();
+        groupType = new GroupType();
+        groupType.setName(name1);
+        Result<GroupType> result = service.save(groupType);
+        assertThat(result.isCalculationFailed()).isFalse();
 
-        assertThat(service.get().size()).isEqualTo(size + 1);
+        resultAll = service.get();
+        assertThat(resultAll.isCalculationFailed()).isFalse();
+        long newSize = resultAll.getResult().size();
 
+        assertThat(newSize).isEqualTo(size + 1);
     }
 
-    // DELETE by NAME --------------------------------------------------------
     @Test
-    public void deleteByName() {
-        groupType.setName(getGroupTypeNameForTesting1());
-        String name = service.saveOrUpdate(groupType).getName();
+    public void whenZeroModelsGetAllReturnEmptySet() {
+        Result<Set<GroupType>> resultAll = service.get();
 
-        assertThat(service.delete(name)).isTrue();
+        assertThat(resultAll).isNotNull();
+        assertThat(resultAll.isCalculationFailed()).isFalse();
+
+        assertThat(resultAll.getResult()).isEqualTo(0);
+    }
+
+    // DELETE by NAME ==========================================================
+    @Test
+    public void deleteByExistNameSuccess() {
+        groupType = new GroupType();
+        groupType.setName(name1);
+
+        Result<GroupType> result = service.save(groupType);
+        assertThat(result.isCalculationFailed()).isFalse();
+
+        String name = result.getResult().getName();
+        Result<Boolean> resultBoolean = service.delete(name);
+        assertThat(result.isCalculationFailed()).isFalse();
+
+        assertThat(resultBoolean.getResult()).isTrue();
+    }
+
+    @Test
+    public void deleteByNotExistNameFailed() {
+        assertThat(service.get(name1).isCalculationFailed()).isTrue();
+
+        Result<Boolean> resultBoolean = service.delete(name1);
+
+        assertThat(resultBoolean.isCalculationFailed()).isTrue();
+        assertThat(resultBoolean.getResult()).isNull();
     }
 
     @Test(expected = NullPointerException.class)
-    public void whenDeleteByNameWithNullThenNPE() {
+    public void whenDeleteByNameNullThenNPE() {
         service.delete((String) null);
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void whenDeleteByNameWithNotExistNameThenIAE() {
-        service.delete("-10L");
+    // GET by NAME ==============================================================
+    @Test
+    public void getByExistNameSuccess() {
+        groupType = new GroupType();
+        groupType.setName(name1);
+
+        Result<GroupType> result = service.save(groupType);
+        assertThat(result.isCalculationFailed()).isFalse();
+
+        result = service.get(name1);
+        assertThat(result.isCalculationFailed()).isFalse();
+
+        assertThat(result.getResult().getName()).isEqualTo(name1);
     }
 
-    // GET by NAME--------------------------------------------------------
     @Test
-    public void getByName() {
-        groupType.setName(getGroupTypeNameForTesting1());
-        String name = service.saveOrUpdate(groupType).getName();
-
-        groupType = service.get(name);
-
-        assertThat(groupType.getName()).isEqualTo(name);
+    public void getByNotExistNameFalse() {
+        Result<GroupType> result = service.get(name1);
+        assertThat(result.isCalculationFailed()).isTrue();
     }
 
     @Test(expected = NullPointerException.class)
-    public void whenGetByNameWithNullThenNPE() {
+    public void whenGetByNameNullThenNPE() {
         service.get((String) null);
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void whenGetByNameWithNotExistNameThenIAE() {
-        service.get("-10L");
+    // IS NAME EXIST ========================================================
+    @Test
+    public void isNameExistReturnTrueWhenNameExist() {
+        groupType = new GroupType();
+        groupType.setName(name1);
+
+        groupType.setName(name1);
+        assertThat(service.save(groupType).isCalculationFailed()).isFalse();
+
+        Result<Boolean> result = service.isNameExist(name1);
+        assertThat(result.isCalculationFailed()).isFalse();
+
+        assertThat(result.getResult()).isTrue();
     }
 
-    // IS NAME EXIST--------------------------------------------------------
     @Test
-    public void isNameExistWithExistingName() {
-        groupType.setName(getGroupTypeNameForTesting1());
-        service.saveOrUpdate(groupType).getName();
+    public void isNameExistReturnFalseWhenNameNotExist() {
+        Result<Boolean> result = service.isNameExist(name1);
+        assertThat(result.isCalculationFailed()).isFalse();
 
-        assertThat(service.isNameExist(getGroupTypeNameForTesting1())).isTrue();
-    }
-
-    @Test
-    public void isNameExistWithNonExistingName() {
-        assertThat(service.isNameExist(getGroupTypeNameForTesting1())).isFalse();
+        assertThat(result.getResult()).isFalse();
     }
 
     @Test(expected = NullPointerException.class)
