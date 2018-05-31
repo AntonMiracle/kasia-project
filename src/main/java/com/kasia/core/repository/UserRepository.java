f16fc92248720490def1f809dd4190ad74222207
Anton Bondarenko <b.anton.m.1986@gmail.com>
2018-05-31 19:51:53 +0200

    Create UserRepositoryImpl. Refactoring UserRepository

--- src/main/java/com/kasia/core/repository/UserRepository.java
+++ src/main/java/com/kasia/core/repository/UserRepository.java
@@ -3,5 +3,7 @@ package com.kasia.core.repository;
 import com.kasia.core.model.User;
 
 public interface UserRepository extends ModelRepository<User> {
+    User get(String username);
 
+    boolean isUsernameExist(String username);
 }
