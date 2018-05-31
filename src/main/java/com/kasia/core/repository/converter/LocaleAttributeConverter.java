813a2ad0eda47761c116ebbee68fd09f12612530
Anton Bondarenko <b.anton.m.1986@gmail.com>
2018-05-31 19:30:22 +0200

    Create LocalAttributeConverterTest

--- src/main/java/com/kasia/core/repository/converter/LocaleAttributeConverter.java
+++ src/main/java/com/kasia/core/repository/converter/LocaleAttributeConverter.java
@@ -4,13 +4,18 @@ import javax.persistence.AttributeConverter;
 import java.util.Locale;
 
 public class LocaleAttributeConverter implements AttributeConverter<Locale, String> {
+    public final String SEPARATOR = ":";
+
     @Override
     public String convertToDatabaseColumn(Locale attribute) {
-        return attribute.toString();
+        String lang = attribute.getLanguage();
+        String country = attribute.getCountry();
+        return lang + SEPARATOR + country;
     }
 
     @Override
     public Locale convertToEntityAttribute(String dbData) {
-        return new Locale(dbData);
+        String[] langCountry = dbData.split(SEPARATOR);
+        return new Locale(langCountry[0], langCountry[1]);
     }
 }
