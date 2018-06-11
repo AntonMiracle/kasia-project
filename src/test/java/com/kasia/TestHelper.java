c665a20e61761efe5e70780d591612a1ac16ef81
Anton Bondarenko <b.anton.m.1986@gmail.com>
2018-06-11 18:22:51 +0200

    Create TestHelper

---
+++ src/test/java/com/kasia/TestHelper.java
@@ -0,0 +1,12 @@
+package com.kasia;
+
+import java.lang.reflect.Field;
+
+public interface TestHelper<T> {
+
+    default void setProtectedId(T where, Object value) throws NoSuchFieldException, IllegalAccessException {
+        Field field = field = where.getClass().getDeclaredField("id");
+        field.setAccessible(true);
+        field.set(where, new Long(0));
+    }
+}
