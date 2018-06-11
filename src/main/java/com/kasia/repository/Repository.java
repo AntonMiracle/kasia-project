b3e3865dbbda25e449d3e2dc9b5e7e782871fe8f
Anton Bondarenko <b.anton.m.1986@gmail.com>
2018-06-11 17:55:56 +0200

    Create Repository.class. Update UserRepository.class

---
+++ src/main/java/com/kasia/repository/Repository.java
@@ -0,0 +1,19 @@
+package com.kasia.repository;
+
+import com.kasia.model.Model;
+import com.kasia.model.result.Result;
+
+import java.util.Set;
+
+public interface Repository<T extends Model> {
+
+    Result<T> add(T model);
+
+    Result<T> remove(long id);
+
+    Result<T> update(T model);
+
+    Result<Set<T>> getAll();
+
+    Result<T> getById(long id);
+}
---
+++ src/main/java/com/kasia/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.kasia.repository;
+
+import com.kasia.model.user.User;
+
+public interface UserRepository extends Repository<User> {
+
+}
