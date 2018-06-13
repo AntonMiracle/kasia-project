a4085c3dd5689c48e87555651f204d241419a130
Anton Bondarenko <b.anton.m.1986@gmail.com>
2018-06-13 22:52:35 +0200

    Model change interface into class. Create test for Model

---
+++ src/test/java/com/kasia/model/ModelTest.java
@@ -0,0 +1,49 @@
+package com.kasia.model;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+import nl.jqno.equalsverifier.Warning;
+import org.junit.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class ModelTest {
+    // CONSTRUCTORS ================================================
+    @Test
+    public void constructorWithNoArgExist() {
+        assertThat(new Model()).isNotNull();
+    }
+
+    @Test
+    public void constructorWithAllArgExist() {
+        int actualSumClassFields = new Model().getClass().getDeclaredFields().length;
+        int expectedSumClassFields = 1;
+
+        long id = 10L;
+
+        assertThat(actualSumClassFields).isEqualTo(expectedSumClassFields);
+        assertThat(new Model(id)).isNotNull();
+    }
+
+    // ID ================================================
+    @Test
+    public void setAndGetId() {
+        Model model = new Model();
+        model.setId(10L);
+        assertThat(model.getId()).isEqualTo(10L);
+    }
+
+    // HASHCODE and EQUALS ================================================
+    @Test
+    public void checkEqualsAndHashCode() {
+        EqualsVerifier.forClass(Model.class)
+                .usingGetClass()
+                .suppress(Warning.NONFINAL_FIELDS)
+                .verify();
+    }
+
+    // TO_STRING ================================================
+    @Test
+    public void toStringIsOverride() {
+        assertThat(new Model().toString().contains("{")).isTrue();
+    }
+}
