6945757928de707c9e4cb16983a854c5033cbc50
Anton Bondarenko <b.anton.m.1986@gmail.com>
2018-06-14 01:56:20 +0200

    Create Article and ArticleTest

---
+++ src/test/java/com/kasia/model/article/ArticleTest.java
@@ -0,0 +1,128 @@
+package com.kasia.model.article;
+
+import com.kasia.model.Model;
+import com.kasia.model.budget.Budget;
+import com.kasia.model.group.Group;
+import com.kasia.model.group.Type;
+import com.kasia.model.item.Item;
+import com.kasia.model.price.Price;
+import com.kasia.model.user.User;
+import nl.jqno.equalsverifier.EqualsVerifier;
+import nl.jqno.equalsverifier.Warning;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class ArticleTest {
+    private Article article;
+
+    @Before
+    public void before() {
+        article = new Article();
+    }
+
+    // CONSTRUCTORS ================================================
+    @Test
+    public void constructorWithNoArgExist() {
+        assertThat(new Article()).isNotNull();
+    }
+
+    @Test
+    public void constructorWithAllArgExist() {
+        int actualSumClassFields = article.getClass().getDeclaredFields().length;
+
+        String description = "description";
+        User maker = new User();
+        Instant create = Instant.now();
+        Budget budget = new Budget();
+        Group group = new Group();
+        Item item = new Item();
+        int quantity = 0;
+        Price price = new Price();
+
+
+        int expectedSumClassFields = 8;
+
+        assertThat(actualSumClassFields).isEqualTo(expectedSumClassFields);
+        assertThat(new Article(maker, description, create, budget, group, item, quantity, price)).isNotNull();
+    }
+
+    // IMPLEMENTS EXTENDS HASHCODE EQUALS TO_STRING ================================================
+    @Test
+    public void extendsModel() {
+        assertThat(Model.class.isAssignableFrom(article.getClass())).isTrue();
+    }
+
+    @Test
+    public void implementsSerializable() {
+        assertThat(Serializable.class.isAssignableFrom(article.getClass())).isTrue();
+    }
+
+    @Test
+    public void checkEqualsAndHashCode() {
+        EqualsVerifier.forClass(article.getClass())
+                .usingGetClass()
+                .suppress(Warning.NONFINAL_FIELDS)
+                .withPrefabValues(Group.class, new Group(), new Group("name", new User(), Type.ARTICLE_INCOME))
+                .verify();
+    }
+
+    @Test
+    public void toStringIsOverride() {
+        assertThat(article.toString().contains("{")).isTrue();
+        assertThat(article.toString().contains(article.getClass().getSimpleName())).isTrue();
+    }
+
+    // GETTERS SETTERS ================================================
+    @Test
+    public void setAndGetDescription() {
+        article.setDescription("text");
+        assertThat(article.getDescription()).isEqualTo("text");
+    }
+
+    @Test
+    public void setAndGetMaker() {
+        User maker = new User();
+        article.setMaker(maker);
+        assertThat(article.getMaker()).isEqualTo(maker);
+    }
+
+    @Test
+    public void setAndGetCreate() {
+        Instant create = Instant.now();
+        article.setCreate(create);
+        assertThat(article.getCreate()).isEqualTo(create);
+    }
+
+    @Test
+    public void setAndGetBudget() {
+        Budget budget = new Budget();
+        article.setBudget(budget);
+        assertThat(article.getBudget()).isEqualTo(budget);
+    }
+
+    @Test
+    public void setAndGetGroup() {
+        Group group = new Group();
+        article.setGroup(group);
+        assertThat(article.getGroup()).isEqualTo(group);
+    }
+
+    @Test
+    public void setAndGetItem() {
+        Item item = new Item();
+        article.setItem(item);
+        assertThat(article.getItem()).isEqualTo(item);
+    }
+
+    @Test
+    public void setAndGetQuantity() {
+        article.setQuantity(2);
+        assertThat(article.getQuantity()).isEqualTo(2);
+    }
+
+}
