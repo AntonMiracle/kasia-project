9ed2f080a1990cc9a3ef70a737d78a46aa5a4579
Anton Bondarenko <b.anton.m.1986@gmail.com>
2018-06-13 23:42:30 +0200

    Update Result and ResultTest

--- src/main/java/com/kasia/model/result/Result.java
+++ src/main/java/com/kasia/model/result/Result.java
@@ -1,9 +1,21 @@
 package com.kasia.model.result;
 
-public class Result<T> {
+import java.io.Serializable;
+
+public class Result<T> implements Serializable {
     private T result;
     private boolean exist;
     private boolean valid;
+
+    public Result() {
+
+    }
+
+    public Result(T result, boolean exist, boolean valid) {
+        this.result = result;
+        this.exist = exist;
+        this.valid = valid;
+    }
 
     public void setResult(T result) {
         this.result = result;
@@ -12,7 +24,6 @@ public class Result<T> {
     public T getResult() {
         return result;
     }
-
 
     public void setExist(boolean exist) {
         this.exist = exist;
@@ -28,5 +39,34 @@ public class Result<T> {
 
     public boolean isValid() {
         return valid;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Result<?> result1 = (Result<?>) o;
+
+        if (exist != result1.exist) return false;
+        if (valid != result1.valid) return false;
+        return result != null ? result.equals(result1.result) : result1.result == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result1 = result != null ? result.hashCode() : 0;
+        result1 = 31 * result1 + (exist ? 1 : 0);
+        result1 = 31 * result1 + (valid ? 1 : 0);
+        return result1;
+    }
+
+    @Override
+    public String toString() {
+        return "Result{" +
+                "result=" + result +
+                ", exist=" + exist +
+                ", valid=" + valid +
+                '}';
     }
 }
--- src/test/java/com/kasia/model/result/ResultTest.java
+++ src/test/java/com/kasia/model/result/ResultTest.java
@@ -1,7 +1,12 @@
 package com.kasia.model.result;
 
+import com.kasia.model.Model;
+import nl.jqno.equalsverifier.EqualsVerifier;
+import nl.jqno.equalsverifier.Warning;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.io.Serializable;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -14,6 +19,36 @@ public class ResultTest {
         result = new Result<>();
         testObject = new TestObject();
         testObject.setName("TetName");
+    }
+
+    // CONSTRUCTORS ================================================
+    @Test
+    public void constructorWithNoArgExist() {
+        assertThat(new Result<TestObject>()).isNotNull();
+    }
+
+    @Test
+    public void constructorWithAllArgExist() {
+        int actualSumClassFields = new Result<TestObject>().getClass().getDeclaredFields().length;
+
+        TestObject testObject = new TestObject();
+        boolean exist = false;
+        boolean valid = false;
+        int expectedSumClassFields = 3;
+
+        assertThat(actualSumClassFields).isEqualTo(expectedSumClassFields);
+        assertThat(new Result<>(testObject, exist, valid)).isNotNull();
+    }
+
+    // ================================================
+    @Test
+    public void notExtendsModel() {
+        assertThat(Model.class.isAssignableFrom(result.getClass())).isFalse();
+    }
+
+    @Test
+    public void implementsSerializable() {
+        assertThat(Serializable.class.isAssignableFrom(result.getClass())).isTrue();
     }
 
     @Test
@@ -34,6 +69,19 @@ public class ResultTest {
         assertThat(result.isValid()).isTrue();
     }
 
+    @Test
+    public void checkEqualsAndHashCode() {
+        EqualsVerifier.forClass(result.getClass())
+                .usingGetClass()
+                .suppress(Warning.NONFINAL_FIELDS)
+                .verify();
+    }
+
+    @Test
+    public void toStringIsOverride() {
+        assertThat(result.toString().contains("{")).isTrue();
+        assertThat(result.toString().contains(result.getClass().getSimpleName())).isTrue();
+    }
 }
 
 class TestObject {
