84309fe8b1b19d3e9c4dd7f6a92f11e490d83abe
Anton Bondarenko <b.anton.m.1986@gmail.com>
2018-06-14 02:09:55 +0200

    Update group.Type and group.TypeTest

--- src/test/java/com/kasia/model/group/TypeTest.java
+++ src/test/java/com/kasia/model/group/TypeTest.java
@@ -6,8 +6,8 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class TypeTest {
     @Test
-    public void userTypeExist() {
-        assertThat(Type.valueOf("USER")).isEqualTo(Type.USER);
+    public void roleTypeExist() {
+        assertThat(Type.valueOf("ROLE")).isEqualTo(Type.ROLE);
     }
 
     @Test
@@ -21,17 +21,38 @@ public class TypeTest {
     }
 
     @Test
+    public void budgetAccessTypeExist() {
+        assertThat(Type.valueOf("BUDGET_ACCESS")).isEqualTo(Type.BUDGET_ACCESS);
+    }
+
+    @Test
     public void itemTypeExist() {
         assertThat(Type.valueOf("ITEM")).isEqualTo(Type.ITEM);
     }
 
     @Test
-    public void consumptionTypeExist() {
+    public void friendUserTypeExist() {
+        assertThat(Type.valueOf("FRIEND_USER")).isEqualTo(Type.FRIEND_USER);
+    }
+
+    @Test
+    public void articleConsumptionTypeExist() {
         assertThat(Type.valueOf("ARTICLE_CONSUMPTION")).isEqualTo(Type.ARTICLE_CONSUMPTION);
     }
+
     @Test
-    public void incomeTypeExist() {
+    public void articleIncomeTypeExist() {
         assertThat(Type.valueOf("ARTICLE_INCOME")).isEqualTo(Type.ARTICLE_INCOME);
+    }
+
+    @Test
+    public void budgetUserTypeExist() {
+        assertThat(Type.valueOf("BUDGET_USER")).isEqualTo(Type.BUDGET_USER);
+    }
+
+    @Test
+    public void synchronizedUserTypeExist() {
+        assertThat(Type.valueOf("SYNCHRONIZED_USER")).isEqualTo(Type.SYNCHRONIZED_USER);
     }
 
 }
