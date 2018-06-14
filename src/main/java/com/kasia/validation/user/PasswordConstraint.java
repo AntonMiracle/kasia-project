014b07207c5ec6549770856b6e7a664ccab412bf
Anton Bondarenko <b.anton.m.1986@gmail.com>
2018-06-14 23:17:05 +0200

    Update UsernameConstraint and Create PasswordConstraint

--- src/main/java/com/kasia/validation/user/PasswordConstraint.java
+++ src/main/java/com/kasia/validation/user/PasswordConstraint.java
@@ -10,23 +10,24 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @NotNull
-@Size(min = 3, max = 16)
+@Size(min = 6, max = 16)
 @Pattern(regexp = "[A-Za-z0-9]+")
 @Constraint(validatedBy = {})
 @Target({METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER})
 @Retention(RUNTIME)
 @ReportAsSingleViolation
-public @interface UsernameConstraint {
-    String message() default "{validation.user.UsernameConstraint.message}";
+public @interface PasswordConstraint {
+    String message() default "{validation.user.PasswordConstraint.message}";
 
     Class<?>[] groups() default {};
 
     Class<? extends Payload>[] payload() default {};
 
-    int min() default 3;
+    int min() default 6;
     int max() default 16;
     String regexp() default "A-Za-z0-9";
 }
