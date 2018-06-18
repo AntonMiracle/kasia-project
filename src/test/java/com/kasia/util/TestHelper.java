174c4aa6744a1f00b0b2496bed921e597aa15078
Anton Bondarenko <b.anton.m.1986@gmail.com>
2018-06-18 14:02:35 +0200

    Update TestHelper

--- src/test/java/com/kasia/util/TestHelper.java
+++ src/test/java/com/kasia/util/TestHelper.java
@@ -3,6 +3,7 @@ package com.kasia.util;
 import javax.validation.Validation;
 import javax.validation.ValidatorFactory;
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -10,19 +11,24 @@ import java.nio.file.StandardCopyOption;
 
 public class TestHelper {
 
-    /**
-     * Make copy ValidationMessages.properties from main/resources to test/resources
-     * Replace if Exist
-     *
-     * @throws IOException
-     */
     public void copyProductionValidationMessagesPropertiesForTesting() throws IOException {
         Path originalValidationMessagesProperties = Paths.get("src", "main", "resources", "ValidationMessages.properties");
         Path testValidationMessagesProperties = Paths.get("src", "test", "resources", "ValidationMessages.properties");
         Files.copy(originalValidationMessagesProperties, testValidationMessagesProperties, StandardCopyOption.REPLACE_EXISTING);
     }
 
-    public ValidatorFactory getValidatorFactory(){
+    public ValidatorFactory getValidatorFactory() {
         return Validation.buildDefaultValidatorFactory();
+    }
+
+    public int countFields(Object where, Class<?> typeOf) {
+        int count = 0;
+        for (Field field : where.getClass().getDeclaredFields()) {
+            if (field.getType().equals(typeOf)) ++count;
+        }
+        return count;
+    }
+    public int countFields(Object where) {
+        return where.getClass().getDeclaredFields().length;
     }
 }
