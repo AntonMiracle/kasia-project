cf7752f3c96613e3188e4e0caf8bfdd390329e80
Anton Bondarenko <b.anton.m.1986@gmail.com>
2018-09-08 15:13:26 +0200

    Update

--- src/main/java/com/kasia/service/imp/EconomyServiceImp.java
+++ src/main/java/com/kasia/service/imp/EconomyServiceImp.java
@@ -1,9 +1,14 @@
 package com.kasia.service.imp;
 
+import com.kasia.model.Budget;
 import com.kasia.model.Economy;
 import com.kasia.service.EconomyService;
 
+import javax.validation.ValidationException;
 import javax.validation.ValidatorFactory;
+import java.math.BigDecimal;
+import java.util.Currency;
+import java.util.Map;
 
 public class EconomyServiceImp implements EconomyService {
     @Override
@@ -13,5 +18,40 @@ public class EconomyServiceImp implements EconomyService {
         try (ValidatorFactory factory = getValidatorFactory()) {
             return factory.getValidator().validate(model).size() == 0;
         }
+    }
+
+    @Override
+    public Economy create(String name) throws ValidationException, NullPointerException {
+        return null;
+    }
+
+    @Override
+    public boolean delete(long id) throws IllegalArgumentException {
+        return false;
+    }
+
+    @Override
+    public boolean update(Economy economy) throws ValidationException, IllegalArgumentException {
+        return false;
+    }
+
+    @Override
+    public Economy getById(long id) throws IllegalArgumentException {
+        return null;
+    }
+
+    @Override
+    public boolean addBudget(Budget budget) throws NullPointerException, IllegalArgumentException {
+        return false;
+    }
+
+    @Override
+    public boolean removeBudget(Budget budget) throws NullPointerException, IllegalArgumentException {
+        return false;
+    }
+
+    @Override
+    public Map<Currency, BigDecimal> getBalance(Economy economy) throws NullPointerException {
+        return null;
     }
 }
