87e854cfacc4aceee903bb6be0f3911433ade44a
Anton Bondarenko <b.anton.m.1986@gmail.com>
2018-09-08 14:45:41 +0200

    Update

--- src/main/java/com/kasia/service/EconomyService.java
+++ src/main/java/com/kasia/service/EconomyService.java
@@ -1,12 +1,29 @@
 package com.kasia.service;
 
+import com.kasia.model.Budget;
 import com.kasia.model.Economy;
 
 import javax.validation.ValidationException;
+import java.math.BigDecimal;
+import java.util.Currency;
+import java.util.Map;
 
 public interface EconomyService extends ValidationService<Economy> {
 
     Economy create(String name) throws ValidationException, NullPointerException;
 
-    boolean delete(Economy economy) throws NullPointerException, IllegalArgumentException;
+    boolean delete(long id) throws IllegalArgumentException;
+
+    boolean update(Economy economy) throws ValidationException, IllegalArgumentException;
+
+    Economy getById(long id) throws IllegalArgumentException;
+
+    boolean addBudget(Budget budget) throws NullPointerException, IllegalArgumentException;
+
+    boolean removeBudget(Budget budget) throws NullPointerException, IllegalArgumentException;
+
+    Map<Currency, BigDecimal> getBalance(Economy economy) throws NullPointerException;
+
+
+
 }
