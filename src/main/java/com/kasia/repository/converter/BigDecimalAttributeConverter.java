a6088461f6fe2a997e6ca70f1e4b49f57331ecad
Anton Bondarenko <b.anton.m.1986@gmail.com>
2018-09-27 02:30:13 +0200

    Create BigDecimalConverter

---
+++ src/main/java/com/kasia/repository/converter/BigDecimalAttributeConverter.java
@@ -0,0 +1,16 @@
+package com.kasia.repository.converter;
+
+import javax.persistence.AttributeConverter;
+import java.math.BigDecimal;
+
+public class BigDecimalAttributeConverter implements AttributeConverter<BigDecimal, String> {
+    @Override
+    public String convertToDatabaseColumn(BigDecimal bigDecimal) {
+        return bigDecimal.toString();
+    }
+
+    @Override
+    public BigDecimal convertToEntityAttribute(String s) {
+        return new BigDecimal(s);
+    }
+}
