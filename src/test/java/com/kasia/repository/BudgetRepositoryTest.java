531194705aa5bb34574ce3c6cc2a3cc45aaa1d93
Anton Bondarenko <b.anton.m.1986@gmail.com>
2018-09-29 23:21:46 +0200

    Update BudgetRepositoryTest

--- src/test/java/com/kasia/repository/BudgetRepositoryTest.java
+++ src/test/java/com/kasia/repository/BudgetRepositoryTest.java
@@ -34,7 +34,6 @@ public class BudgetRepositoryTest extends RepositoryTestHelper {
         budgetRepository.save(budget);
 
         assertThat(budgetRepository.getById(budget.getId())).isEqualTo(budget);
-
     }
 
     @Test
