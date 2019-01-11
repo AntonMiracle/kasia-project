8443f875638d10a5a1c90f838a21dfb1a0d97ce9
Anton Bondarenko <b.anton.m.1986@gmail.com>
2019-01-11 16:41:35 +0100

    Change EntityManagerProducer. Make that producer return new entity manager every time when call

--- src/main/java/com/kasia/producer/EntityManagerProducer.java
+++ src/main/java/com/kasia/producer/EntityManagerProducer.java
@@ -1,17 +1,16 @@
 package com.kasia.producer;
 
-import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Produces;
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceContext;
 
 public class EntityManagerProducer {
     @PersistenceContext(unitName = "UnitForTest")
-    private EntityManager em;
+    private EntityManagerFactory emf;
 
     @Produces
-    @RequestScoped
     public EntityManager entityManager() {
-        return em;
+        return emf.createEntityManager();
     }
 }
