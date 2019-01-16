346d451d3341fbd068e0ba0250cbd1e7d439f57c
Anton Bondarenko <b.anton.m.1986@gmail.com>
2019-01-16 19:12:34 +0100

    Finish create and update BudgetService + Imp and tests. Build Success

--- src/main/java/com/kasia/model/service/imp/BudgetServiceImp.java
+++ src/main/java/com/kasia/model/service/imp/BudgetServiceImp.java
@@ -1,42 +1,53 @@
 package com.kasia.model.service.imp;
 
+import com.kasia.exception.IdRuntimeException;
+import com.kasia.model.Balance;
 import com.kasia.model.Budget;
-import com.kasia.model.User;
+import com.kasia.model.repository.BudgetRepository;
 import com.kasia.model.service.BudgetService;
 import com.kasia.validation.ValidationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 @Service
 public class BudgetServiceImp implements BudgetService, ValidationService<Budget> {
+    @Autowired
+    private BudgetRepository budgetRepository;
+
     @Override
     public Budget save(Budget model) {
-        return null;
+        budgetRepository.save(model);
+        return model;
     }
 
     @Override
     public boolean delete(Budget model) {
-        return false;
+        if (model.getId() <= 0) throw new IdRuntimeException();
+        budgetRepository.delete(model);
+        return true;
     }
 
     @Override
     public Budget findById(long id) {
-        return null;
+        if (id <= 0) throw new IdRuntimeException();
+        Optional<Budget> budget = budgetRepository.findById(id);
+        return budget.isPresent() ? budget.get() : null;
     }
 
     @Override
     public Set<Budget> findAll() {
-        return null;
+        Set<Budget> budgets = new HashSet<>();
+        budgetRepository.findAll().forEach(budgets::add);
+        return budgets;
     }
 
     @Override
-    public boolean isNameUnique(User user, String name) {
-        return false;
-    }
-
-    @Override
-    public Budget create(String name) {
-        return null;
+    public Budget create(String name, Balance balance) {
+        return new Budget(name, balance, LocalDateTime.now().withNano(0));
     }
 }
---
+++ src/test/java/com/kasia/model/service/BudgetServiceIT.java
@@ -0,0 +1,103 @@
+package com.kasia.model.service;
+
+import com.kasia.ModelTestData;
+import com.kasia.exception.IdRuntimeException;
+import com.kasia.model.Budget;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BudgetServiceIT {
+    @Autowired
+    private BudgetService budgetService;
+
+    @After
+    public void after() {
+        for (Budget budget : budgetService.findAll()) budgetService.delete(budget);
+    }
+
+    @Test
+    public void create() throws Exception {
+        Budget budget = ModelTestData.getBudget1();
+        Budget createdBudget = budgetService.create(budget.getName(), budget.getBalance());
+
+        assertThat(createdBudget.getId() == 0).isTrue();
+        assertThat(createdBudget.getName()).isEqualTo(budget.getName());
+        assertThat(createdBudget.getBalance()).isEqualTo(budget.getBalance());
+        assertThat(createdBudget.getCreateOn()).isNotNull();
+        assertThat(createdBudget.getCreateOn().compareTo(LocalDateTime.now().plusSeconds(2)) < 0).isTrue();
+    }
+
+    @Test
+    public void saveNewBudget() {
+        Budget budget = ModelTestData.getBudget1();
+        assertThat(budget.getId() == 0).isTrue();
+
+        budgetService.save(budget);
+
+        assertThat(budget.getId() > 0).isTrue();
+        assertThat(budgetService.findById(budget.getId())).isNotNull();
+    }
+
+    @Test
+    public void saveUpdateBudget() {
+        Budget budget = ModelTestData.getBudget1();
+        budgetService.save(budget);
+        String newName = "newName";
+        long id = budget.getId();
+
+        budget.setName(newName);
+        budgetService.save(budget);
+
+        assertThat(budgetService.findById(id).getName()).isEqualTo(newName);
+    }
+
+    @Test
+    public void findAll() {
+        assertThat(budgetService.findAll().size() == 0).isTrue();
+        budgetService.save(ModelTestData.getBudget1());
+        budgetService.save(ModelTestData.getBudget1());
+
+        assertThat(budgetService.findAll().size() == 2).isTrue();
+    }
+
+    @Test
+    public void delete() {
+        Budget budget = ModelTestData.getBudget1();
+        budgetService.save(budget);
+
+        budgetService.delete(budget);
+
+        assertThat(budgetService.findById(budget.getId())).isNull();
+    }
+
+    @Test
+    public void findById() {
+        Budget budget = ModelTestData.getBudget1();
+
+        budgetService.save(budget);
+
+        assertThat(budgetService.findById(budget.getId())).isEqualTo(budget);
+        assertThat(budgetService.findById(budget.getId() + 1)).isNull();
+    }
+
+    @Test(expected = IdRuntimeException.class)
+    public void whenFindByIdWithZeroIdThenException() {
+        assertThat(budgetService.findById(0)).isNotNull();
+    }
+
+    @Test(expected = IdRuntimeException.class)
+    public void whenFindByIdWithNegativeIdThenException() {
+        assertThat(budgetService.findById(-1)).isNotNull();
+    }
+
+}
