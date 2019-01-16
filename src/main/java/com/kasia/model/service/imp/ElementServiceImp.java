884f9d04d33771a3926469b0b17e101377787706
Anton Bondarenko <b.anton.m.1986@gmail.com>
2019-01-16 20:33:55 +0100

    Create ElementServiceImp + tests. Build Success

--- src/main/java/com/kasia/model/service/imp/ElementServiceImp.java
+++ src/main/java/com/kasia/model/service/imp/ElementServiceImp.java
@@ -1,38 +1,52 @@
 package com.kasia.model.service.imp;
 
+import com.kasia.exception.IdRuntimeException;
 import com.kasia.model.Element;
 import com.kasia.model.ElementType;
 import com.kasia.model.Price;
+import com.kasia.model.repository.ElementRepository;
 import com.kasia.model.service.ElementService;
 import com.kasia.validation.ValidationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 @Service
-public class ElementServiceImp implements ElementService, ValidationService<Element>{
+public class ElementServiceImp implements ElementService, ValidationService<Element> {
+    @Autowired
+    private ElementRepository elementRepository;
+
     @Override
     public Element save(Element model) {
-        return null;
+        return elementRepository.save(model);
     }
 
     @Override
     public boolean delete(Element model) {
-        return false;
+        if (model.getId() <= 0) throw new IdRuntimeException();
+        elementRepository.delete(model);
+        return true;
     }
 
     @Override
     public Element findById(long id) {
-        return null;
+        if (id <= 0) throw new IdRuntimeException();
+        Optional<Element> element = elementRepository.findById(id);
+        return element.isPresent() ? element.get() : null;
     }
 
     @Override
     public Set<Element> findAll() {
-        return null;
+        Set<Element> elements = new HashSet<>();
+        elementRepository.findAll().forEach(elements::add);
+        return elements;
     }
 
     @Override
     public Element create(String name, ElementType type, Price defaultPrice) {
-        return null;
+        return new Element(name, defaultPrice, type);
     }
 }
---
+++ src/test/java/com/kasia/model/service/ElementServiceIT.java
@@ -0,0 +1,115 @@
+package com.kasia.model.service;
+
+import com.kasia.ModelTestData;
+import com.kasia.exception.IdRuntimeException;
+import com.kasia.model.Element;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ElementServiceIT {
+    @Autowired
+    private ElementService elementService;
+
+    @After
+    public void after() {
+        for (Element element : elementService.findAll()) elementService.delete(element);
+    }
+
+    @Test
+    public void create() throws Exception {
+        Element element = ModelTestData.getElement1();
+        Element create = elementService.create(element.getName(), element.getType(), element.getDefaultPrice());
+
+        assertThat(create).isEqualTo(element);
+    }
+
+    @Test
+    public void saveNew() {
+        Element expected = ModelTestData.getElement1();
+        assertThat(expected.getId() == 0).isTrue();
+
+        elementService.save(expected);
+        Element actual = elementService.findById(expected.getId());
+
+        assertThat(expected.getId() > 0).isTrue();
+        assertThat(actual).isNotNull();
+        assertThat(actual.getDefaultPrice()).isEqualTo(expected.getDefaultPrice());
+        assertThat(actual.getName()).isEqualTo(expected.getName());
+        assertThat(actual.getType()).isEqualTo(expected.getType());
+    }
+
+    @Test
+    public void saveUpdate() {
+        Element element = ModelTestData.getElement1();
+        elementService.save(element);
+        String newName = "newName";
+        long id = element.getId();
+
+        element.setName(newName);
+        elementService.save(element);
+
+        assertThat(elementService.findById(id).getName()).isEqualTo(newName);
+    }
+
+    @Test
+    public void findAll() {
+        assertThat(elementService.findAll().size() == 0).isTrue();
+        elementService.save(ModelTestData.getElement1());
+        elementService.save(ModelTestData.getElement2());
+
+        assertThat(elementService.findAll().size() == 2).isTrue();
+    }
+
+    @Test
+    public void delete() {
+        Element element = ModelTestData.getElement1();
+        elementService.save(element);
+
+        elementService.delete(element);
+
+        assertThat(elementService.findById(element.getId())).isNull();
+    }
+
+    @Test(expected = IdRuntimeException.class)
+    public void whenDeleteWithZeroIdThenException() {
+        Element element = ModelTestData.getElement1();
+        element.setId(0);
+        elementService.delete(element);
+    }
+
+    @Test(expected = IdRuntimeException.class)
+    public void whenDeleteWithNegativeIdThenException() {
+        Element element = ModelTestData.getElement1();
+        element.setId(-1);
+        elementService.delete(element);
+    }
+
+    @Test
+    public void findById() {
+        Element element = ModelTestData.getElement1();
+
+        elementService.save(element);
+
+        assertThat(elementService.findById(element.getId())).isEqualTo(element);
+        assertThat(elementService.findById(element.getId() + 1)).isNull();
+    }
+
+    @Test(expected = IdRuntimeException.class)
+    public void whenFindByIdWithZeroIdThenException() {
+        assertThat(elementService.findById(0)).isNotNull();
+    }
+
+    @Test(expected = IdRuntimeException.class)
+    public void whenFindByIdWithNegativeIdThenException() {
+        assertThat(elementService.findById(-1)).isNotNull();
+    }
+
+}
