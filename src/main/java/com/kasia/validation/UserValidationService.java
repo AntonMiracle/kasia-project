b266622d4d9cc6a6d08edcfd2669230310b0ae1c
Anton Bondarenko <b.anton.m.1986@gmail.com>
2019-01-17 13:16:00 +0100

    Create UserValidationService

---
+++ src/main/java/com/kasia/validation/UserValidationService.java
@@ -0,0 +1,12 @@
+package com.kasia.validation;
+
+import com.kasia.model.User;
+
+public interface UserValidationService extends ValidationService<User> {
+
+    boolean isPasswordValid(String password);
+
+    boolean isEmailValid(String email);
+
+    boolean isNameValid(String name);
+}
