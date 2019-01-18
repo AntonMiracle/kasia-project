f218115cbef094825777aaf6b86dc542065e8048
Anton Bondarenko <b.anton.m.1986@gmail.com>
2019-01-18 22:39:08 +0100

    Rename IdRuntimeException

---
+++ src/main/java/com/kasia/exception/IdInvalidRuntimeException.java
@@ -0,0 +1,7 @@
+package com.kasia.exception;
+
+public class IdInvalidRuntimeException extends RuntimeException {
+    public IdInvalidRuntimeException() {
+        super();
+    }
+}
--- src/main/java/com/kasia/exception/IdRuntimeException.java
+++
@@ -1,7 +0,0 @@
-package com.kasia.exception;
-
-public class IdRuntimeException extends RuntimeException {
-    public IdRuntimeException() {
-        super();
-    }
-}
