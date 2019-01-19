f01d31155ead9720f3ed501611e3cfcb1dfe7695
Anton Bondarenko <b.anton.m.1986@gmail.com>
2019-01-19 20:45:50 +0100

    Update IntervalRuntimeException

---
+++ src/main/java/com/kasia/exception/IntervalRuntimeException.java
@@ -0,0 +1,7 @@
+package com.kasia.exception;
+
+public class IntervalRuntimeException extends RuntimeException {
+    public IntervalRuntimeException() {
+        super();
+    }
+}
--- src/main/java/com/kasia/exception/PeriodLocalDateTimeException.java
+++
@@ -1,7 +0,0 @@
-package com.kasia.exception;
-
-public class PeriodLocalDateTimeException extends RuntimeException {
-    public PeriodLocalDateTimeException() {
-        super();
-    }
-}
