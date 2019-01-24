3bd9f6fd813ad1c6289a33c3182791b2be4726e6
Anton Bondarenko <b.anton.m.1986@gmail.com>
2019-01-24 21:32:34 +0100

    Update LoginController fix redirect from login.html when user is auth

--- src/main/java/com/kasia/controller/LoginController.java
+++ src/main/java/com/kasia/controller/LoginController.java
@@ -5,14 +5,12 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @EnableAutoConfiguration
-@RequestMapping("/login")
 public class LoginController {
 
-    @GetMapping
+    @GetMapping({"/login", "/"})
     public String enter() {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         if (auth == null || auth.getName().equals("anonymousUser")) return "login";
