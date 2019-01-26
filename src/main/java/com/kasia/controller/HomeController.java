3577bd0de0ae17b590a39ac704970c73d630f75c
Anton Bondarenko <b.anton.m.1986@gmail.com>
2019-01-26 18:19:25 +0100

    Update messages.properties , home.html , tables.html , HomeController

--- src/main/java/com/kasia/controller/HomeController.java
+++ src/main/java/com/kasia/controller/HomeController.java
@@ -1,18 +1,30 @@
 package com.kasia.controller;
 
+import com.kasia.model.User;
+import com.kasia.model.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
+
 @Controller
 @RequestMapping("home")
 public class HomeController {
+    @Autowired
+    private UserService uService;
     private final String PROFILE_ATTRIBUTE_NAME = "profileAtt";
+    private final String USER_ATTRIBUTE_NAME = "user";
 
     @GetMapping("profile")
-    public String budgetAdd(Model model) {
+    public String openProfile(Model model, Principal principal) {
+        User user = uService.findUserByEmail(principal.getName());
+        System.out.println("============== # openProfile user:");
+        System.out.println(user);
         model.addAttribute(PROFILE_ATTRIBUTE_NAME, PROFILE_ATTRIBUTE_NAME);
+        model.addAttribute(USER_ATTRIBUTE_NAME, user);
         return "home";
     }
 
