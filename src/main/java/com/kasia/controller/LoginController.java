5e6aaae39cf6ab0eef20c2d7ee1c88a87ea1e5fa
Anton Bondarenko <b.anton.m.1986@gmail.com>
2019-03-06 17:22:53 +0100

    Update

--- src/main/java/com/kasia/controller/LoginController.java
+++ src/main/java/com/kasia/controller/LoginController.java
@@ -1,6 +1,5 @@
 package com.kasia.controller;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import static com.kasia.controller.ViewNameAndControllerURL.*;
 
 @Controller
-@EnableAutoConfiguration
 public class LoginController {
 
     @GetMapping({U_LOGIN, U_ROOT})
